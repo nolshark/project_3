e118b5c25430b7fd3b1ae572fa9aeddec5bb1217
msalamey <m.salamey23@gmail.com>
2022-12-19 20:04:05 -0500

    minor changes made to file structure

--- models/index.js
+++
@@ -1,4 +0,0 @@
-const User = require('./User');
-const Character = require('./Character');
-
-module.exports = { User, Character };
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || '', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect('mongodb://127.0.0.1:27017/characterDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
