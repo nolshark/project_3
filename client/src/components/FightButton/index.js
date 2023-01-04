13956674ec75cf0e498a3ac42999bb6a7e9d25f1
msalamey <m.salamey23@gmail.com>
2023-01-03 20:58:43 -0500

    added buttons

--- client/src/components/FightButton/index.js
+++ client/src/components/FightButton/index.js
@@ -1,1 +1,19 @@
 import React from "react";
+
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
+
+function fight() {
+    alert('Fight!');
+  }
+
+// Usage
+<Button onClick={fight}></Button>
