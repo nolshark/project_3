23577c70149bd77eedde6d49e5d5a8637404852c
msalamey <m.salamey23@gmail.com>
2023-01-05 21:32:42 -0500

    added home.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+const Home = () => {
+
+    
+    return (
+        
+        <main>
+            <div>
+                <h1>This is the HomePage</h1>
+            </div>
+        </main>
+    );
+};
+
+export default Home;
