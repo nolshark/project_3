e94a1c81c8e75439cedfc151895eb2d244b35a26
msalamey <m.salamey23@gmail.com>
2023-01-07 13:47:07 -0500

    added apollo to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Fight from './pages/Fight';
@@ -9,36 +16,60 @@ import Stats from './pages/Stats';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+const httpLink = createHttpLink({
+    uri: '/graphql',
+  });
+
+const authLink = setContext((_, { headers }) => {
+// get the authentication token from local storage if it exists
+const token = localStorage.getItem('id_token');
+// return the headers to the context so httpLink can read them
+return {
+    headers: {
+    ...headers,
+    authorization: token ? `Bearer ${token}` : '',
+    },
+};
+});
+
+const client = new ApolloClient({
+// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+link: authLink.concat(httpLink),
+cache: new InMemoryCache(),
+});
+
 function App () {
     return(
-        <Router>
-            <Header />
-            <div>
-                <Routes>
-                    <Route 
-                        path="/"
-                        element={<Home />}
-                    />
-                    <Route 
-                        path="/login"
-                        element={<Login />}
-                    />
-                    <Route 
-                        path="/signup"
-                        element={<Signup />}
-                    />
-                    <Route 
-                        path="/fight"
-                        element={<Fight />}
-                    />
-                    <Route 
-                        path="/stats"
-                        element={<Stats />}
-                    />
-                </Routes>
-            </div>
-            <Footer />
-        </Router>
+        <ApolloProvider client={client}>
+            <Router>
+                <Header />
+                <div>
+                    <Routes>
+                        <Route 
+                            path="/"
+                            element={<Home />}
+                        />
+                        <Route 
+                            path="/login"
+                            element={<Login />}
+                        />
+                        <Route 
+                            path="/signup"
+                            element={<Signup />}
+                        />
+                        <Route 
+                            path="/fight"
+                            element={<Fight />}
+                        />
+                        <Route 
+                            path="/stats"
+                            element={<Stats />}
+                        />
+                    </Routes>
+                </div>
+                <Footer />
+            </Router>
+        </ApolloProvider>
     );
 }
 
