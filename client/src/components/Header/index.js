cd61ba09007a92f6c6e2980f8e3ce92af44eccab
cgarnica92 <c_garnica92@yahoo.com>
2023-01-07 12:41:18 -0500

    Updated Header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { useLocation } from "react-router-dom";
 
 const Header  = () => {
+    let location = useLocation();
 
     return (
         <header class="columns is-centered is-full">
