24b8eb8212cbba35c12ce13287afdfff9cbb7cd5
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2023-01-06 20:09:44 -0500

    added process.env

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/characterDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/characterDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
