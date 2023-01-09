2d9ed16b5a4b2e03dc63c6d5de69527b3a7a0c8c
msalamey <m.salamey23@gmail.com>
2023-01-08 20:22:39 -0500

    updated auth and minor changes to components

--- client/src/components/CharCard/index.js
+++ client/src/components/CharCard/index.js
@@ -5,12 +5,13 @@ const CharCard = () => {
     return (
         <div> 
             <div id="charImage"> 
-            
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"></img>
             </div>
             <div id="charDescribe">
                 <div id="charBio">
                 </div>
                 <div id="charAttr">
+
                 </div>
             </div>   
         </div>
